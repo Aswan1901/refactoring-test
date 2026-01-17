df3fe5dcd20735342c601f34bbace332317ca7ad
Aswan1901 <aswan.josephmathieu1999@gmail.com>
2026-01-17 18:02:12 +0100

    total order by customer

---
+++ src/TotalOrderRepository.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+
+use App\DataRepository;
+
+class TotalOrderRepository
+    {
+        public function getTotalOrders():array
+        {
+
+
+
+            $repo = new DataRepository();
+            //recupere les customers et orders
+            $orders = $repo->loadOrders();
+            $customers = $repo->loadCustomers();
+
+            $totalOrders=[];
+
+            foreach ($customers as $customer) {
+                $customersById[$customer['id']] = $customer;
+            }
+
+            foreach ($orders as $order) {
+                $customerId = $order['customer_id'];
+                $unitPrice = $order['unit_price'];
+
+                if (!isset($customersById[$customerId])) {
+                    continue;
+                }
+
+                if (!isset($totalOrders[$customerId])) {
+                    $totalOrders[$customerId] = 0;
+                }
+
+                $totalOrders[$customerId] += $unitPrice;
+
+            }
+
+            return $totalOrders;
+        }
+    }
+
+    try{
+
+    $data = new TotalOrderRepository();
+    $totalOrders = $data->getTotalOrders();
+    print_r($totalOrders);
+
+    }catch(\Exception $e){
+    echo "ERROR: " . $e->getMessage() . "\n";
+    }
