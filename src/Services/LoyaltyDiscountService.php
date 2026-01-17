78691beaefa424ffb88f64ebcbab1fb4e12e7f9b
Aswan1901 <aswan.josephmathieu1999@gmail.com>
2026-01-17 21:26:44 +0100

    modified loyaltyDiscount

--- src/Services/LoyaltyDiscountService.php
+++ src/Services/LoyaltyDiscountService.php
@@ -7,24 +7,29 @@ use App\Config\CalculConfig;
 
 final class LoyaltyDiscountService
 {
-    public function calculateLoyaltyDiscount(): array
+    public function calculateLoyaltyPoints(array $orders): int
     {
-        $loyaltyPoints = [];
-
-        $repo = new DataRepository();
-        $orders = $repo->loadOrders();
+        $points = 0;
 
         foreach ($orders as $order) {
-            $cid = $order['customer_id'];
-
-            if (!isset($loyaltyPoints[$cid])) {
-                $loyaltyPoints[$cid] = 0;
-            }
-
-            $loyaltyPoints[$cid] +=
-                $order['quantity'] * $order['unit_price'] * CalculConfig::LOYALTY_RATIO;
+            $qty = $order['qty'] ?? 1;
+            $points += $qty * $order['unit_price'] * CalculConfig::LOYALTY_RATIO;
         }
 
-        return $loyaltyPoints;
+        return $points;
+    }
+
+    public function calculateLoyaltyDiscount(int $points): float
+    {
+        if ($points > 500) {
+            return min($points * 0.15, 100.0);
+        }
+
+        if ($points > 100) {
+            return min($points * 0.10, 50.0);
+        }
+
+        return 0.0;
     }
 }
+
